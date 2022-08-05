442e7b14ecb0f2062987ba6cc111404b023309bc
Castelani <101222446+Castel4n1@users.noreply.github.com>
2022-08-04 21:50:52 -0300

    View

--- IngressoMVC/Models/ViewModels/RequestDTO/PostCinemaDTO.cs
+++ IngressoMVC/Models/ViewModels/RequestDTO/PostCinemaDTO.cs
@@ -11,7 +11,9 @@ namespace IngressoMVC.Models.ViewModels.RequestDTO
         [Required(ErrorMessage ="É Necessário Utilizar um nome")]
         [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage ="Max 50, Min 3 Caracteres.")]
         public string Nome { get;  set; }
+
         public string Descricao { get;  set; }
+
         [Required(ErrorMessage ="Obrigatório Logo")]
         public string LogoURL { get;  set; }
     }
