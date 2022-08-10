e40a8158294b40d2ceb55069eab3584ce3c36679
Castelani <kaue@presconinformatica.com.br>
2022-08-10 15:23:34 -0300

    REFORMULAÇÃO VIEWMODELS

---
+++ IngressoMVC/Models/ViewModels/RequestDTO/PostFilmeDropdownDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace IngressoMVC.Models.ViewModels.RequestDTO
+{
+    public class PostFilmeDropdownDTO
+    {
+        public PostFilmeDropdownDTO()
+        {
+            Cinemas = new List<Cinema>();
+            Produtores = new List<Produtor>();
+            Atores = new List<Ator>();
+            Categorias = new List<Categoria>();
+        }
+
+        public List<Cinema> Cinemas { get; set; }
+        public List<Produtor> Produtores { get; set; }
+        public List<Ator> Atores { get; set; }
+        public List<Categoria> Categorias { get; set; }
+    }
+}
