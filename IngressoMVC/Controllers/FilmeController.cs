9bc40fca4c084fa0b12a7628ee791d03e1b8a3c2
Castelani <kaue@presconinformatica.com.br>
2022-08-11 17:59:49 -0300

    atualização

--- IngressoMVC/Controllers/FilmeController.cs
+++ IngressoMVC/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using IngressoMVC.Models;
 using IngressoMVC.Models.ViewModels.RequestDTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -39,6 +40,8 @@ namespace IngressoMVC.Controllers
 
         public IActionResult Criar()
         {
+            DadosDropdown();
+
             return View();
         }
 
@@ -76,17 +79,36 @@ namespace IngressoMVC.Controllers
                 _context.SaveChanges();
             }
 
-            return RedirectToAction(nameof(Index));
+            DadosDropdown();
+            return View();
         }
 
         public IActionResult Atualizar(int id)
         {
-            var result = _context.Filmes.FirstOrDefault(x => x.Id == id);
-            
-                if (result == null)
+            var result = _context.Filmes
+            .Include(x => x.AtoresFilmes).ThenInclude(x => x.Ator)
+            .Include(x => x.FilmesCategorias).ThenInclude(x => x.Categoria)
+            .FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
                 return View("NotFound");
 
-            return View(result);
+            var response = new PostFilmeDTO()
+            {
+                Titulo = result.Titulo,
+                Descricao = result.Descricao,
+                Preco = result.Preco,
+                ImageURL = result.ImageURL,
+                DataLancamento = result.DataLancamento,
+                DataEncerramento = result.DataEncerramento,
+                CinemaId = result.CinemaId,
+                ProdutorId = result.ProdutorId,
+                AtoresId = result.AtoresFilmes.Select(x => x.AtorId).ToList(),
+                CategoriasId = result.FilmesCategorias.Select(x => x.CategoriaId).ToList()
+            };
+
+            DadosDropdown();
+            return View(response);
         }
         [HttpPost]
         public IActionResult Atualizar(int id, PostFilmeDTO filmeDto)
@@ -94,15 +116,27 @@ namespace IngressoMVC.Controllers
             var result = _context.Filmes.FirstOrDefault(x => x.Id == id);
 
             if (!ModelState.IsValid)
+            {
+                DadosDropdown();
                 return View(result);
+            }
 
-            result.AlterarDados(filmeDto.Titulo, filmeDto.Descricao, filmeDto.Preco, filmeDto.ImageURL, filmeDto.ProdutorId, filmeDto.CinemaId, filmeDto.DataEncerramento, filmeDto.DataLancamento) ;
-
+            result.AlterarDados
+                (
+                filmeDto.Titulo,
+                filmeDto.Descricao,
+                filmeDto.Preco,
+                filmeDto.ImageURL,
+                filmeDto.ProdutorId,
+                filmeDto.CinemaId,
+                filmeDto.DataEncerramento,
+                filmeDto.DataEncerramento
+                );
             _context.Update(result);
             _context.SaveChanges();
-
-            return RedirectToAction(nameof(Index), result);
+            return RedirectToAction(nameof(Detalhes), result);
         }
+
         public IActionResult Deletar(int id)
         {
             var result = _context.Filmes.FirstOrDefault(x => x.Id == id);
@@ -112,6 +146,7 @@ namespace IngressoMVC.Controllers
 
             return View(result);
         }
+
         [HttpPost, ActionName("Deletar")]
         public IActionResult ConfirmarDeletar(int id)
         {
@@ -122,6 +157,53 @@ namespace IngressoMVC.Controllers
 
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public void DadosDropdown()
+        {
+            var resp = new PostFilmeDropdownDTO()
+            {
+                Atores = _context.Atores.OrderBy(x => x.Nome).ToList(),
+                Categorias = _context.Categorias.OrderBy(x => x.Nome).ToList(),
+                Cinemas = _context.Cinemas.OrderBy(x => x.Nome).ToList(),
+                Produtores = _context.Produtores.OrderBy(x => x.Nome).ToList()
+            };
+
+            ViewBag.Atores = new SelectList(resp.Atores, "Id", "Nome");
+            ViewBag.Categorias = new SelectList(resp.Categorias, "Id", "Nome");
+            ViewBag.Cinemas = new SelectList(resp.Cinemas, "Id", "Nome");
+            ViewBag.Produtores = new SelectList(resp.Produtores, "Id", "Nome");
+        }
+        public IActionResult Buscar(string filtroDeBsuca)
+        {
+            var result = _context.Filmes
+                .Include(filme => filme.AtoresFilmes).ThenInclude(atorFilme => atorFilme.Ator)
+                .ToList();
+
+            /*
+            Filmes
+	            Onde
+		            (Filme.Titulo.Contem("busca")) ou
+		            (Filme.Descri.Contem("busca)) ou
+		            (Filme.AtoresFilmes.Atores.Nome.Contem("busca")) =[SOLUÇÃO ATIVIDADE]=
+            */
+
+            if (!string.IsNullOrEmpty(filtroDeBsuca))
+            {
+                filtroDeBsuca = filtroDeBsuca.ToLower().Trim();
+
+                var resultadoDaBusca = result
+                    .Where(filme =>
+                        filme.Titulo.ToLower().Contains(filtroDeBsuca) ||
+                        filme.Descricao.ToLower().Contains(filtroDeBsuca) ||
+                        filme.AtoresFilmes.Select(af => af.Ator.Nome.ToLower().Contains(filtroDeBsuca)).FirstOrDefault())
+                    .OrderBy(x => x.Titulo)
+                    .ToList();
+
+                return View(nameof(Index), resultadoDaBusca);
+            }
+
+            return View(nameof(Index), result);
         }
     }
 }
